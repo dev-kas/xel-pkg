83dddd63f4100603cd8f68a4916ae4294068ffec
KAS <krishaang_1808@hotmail.com>
2025-06-27 22:56:22 +0530

    refactor: remove package indexing from server startup
    - Remove direct package indexing call from server startup
    - Clean up commented-out error handling code
    - Improve code organization and separation of concerns

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import './common/env.js';
-import indexPackage from './package_indexing/index.js';
 import connectDB from './db.js';
 
 import Server from './common/server.js';
@@ -7,13 +6,8 @@ import routes from './routes.js';
 
 async function main() {
   await connectDB();
-  indexPackage('https://github.com/dev-kas/xel-rockets', 'anything@inbox.test');
 
   new Server().router(routes).listen(process.env.PORT || 3000);
 }
 
 main();
-// .catch((err) => {
-//   console.error('Error:', err);
-//   process.exit(1);
-// });
